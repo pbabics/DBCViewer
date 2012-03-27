406c947d490725ed0f3157c76f4f7b1770180469
NTX <NTX@zoznam.sk>
2012-03-27 16:54:59 +0200

    Implement option to Get/Set format string and to reload file

--- dbcviewer.cpp
+++ dbcviewer.cpp
@@ -27,7 +27,9 @@ void DBCViewer::on_actionOpen_triggered()
 
 void DBCViewer::LoadIntoTable(QString file)
 {
-    QFile loader(file);
+    if (loader.isOpen())
+        loader.close();
+    loader.setFileName(file);
     if (!loader.open(QIODevice::ReadOnly))
     {
         QMessageBox message;
@@ -42,7 +44,6 @@ void DBCViewer::LoadIntoTable(QString file)
     int fields;
     int recordSize;
     int stringsSize;
-    int stringsOffset;
 
     QDataStream stream(&loader);
     stream.setByteOrder(QDataStream::LittleEndian);
@@ -53,6 +54,129 @@ void DBCViewer::LoadIntoTable(QString file)
         message.setText("This it not DBC File !!");
         message.exec();
         ui->statusBar->showMessage("File is not DBC " + file);
+        return;
+    }
+    lastFile = file;
+
+    stream >> records;
+    stream >> fields;
+    stream >> recordSize;
+    stream >> stringsSize;
+    printf("File: '%s'  Records: %d  fields: %d  recordSize: %d  stringsSize: %d\n", file.toAscii().constData(), records, fields, recordSize, stringsSize);
+    for (int i = ui->tableWidget->columnCount(); i < fields; ++i)
+        ui->tableWidget->insertColumn(ui->tableWidget->columnCount());
+
+    for (int i = ui->tableWidget->rowCount(); i < records; ++i)
+        ui->tableWidget->insertRow(ui->tableWidget->rowCount());
+
+    for (int i = ui->tableWidget->columnCount(); i > fields; --i)
+        ui->tableWidget->removeColumn(ui->tableWidget->columnCount() - 1);
+
+    for (int i = ui->tableWidget->rowCount(); i > records; --i)
+        ui->tableWidget->removeRow(ui->tableWidget->rowCount() - 1);
+
+    int* table = new int[records * recordSize];
+
+    for (int i = 0; i < records; ++i)
+    {
+        stream.readRawData((char*)(table + i * recordSize), recordSize);
+    }
+
+    char* strings = new char[stringsSize];
+    stream.readRawData(strings, stringsSize);
+
+    ui->tableWidget->setUpdatesEnabled(false);
+
+    fieldTypes= QString(fields, 's');
+    fieldTypes[0] = 'n';
+
+    for (register int i = 0; i < records; ++i)
+    {
+        for (register int b = 1; b < fields; ++b)
+        {
+            if (table[i * recordSize + b] > 0 &&
+                table[i * recordSize + b] < stringsSize  &&
+                *(strings + table[i * recordSize + b] - 1) == 0 &&
+                *(strings + table[i * recordSize + b]) >= 'A' &&
+                *(strings + table[i * recordSize + b]) <= 'z' &&
+                fieldTypes[b] == 's')
+                fieldTypes[b] = 's';
+            else
+            {
+                if (*(float*)(table + i * recordSize + b) - int(*(float*)(table + i * recordSize + b)) != 0 && log10(table[i * recordSize + b]) > 6)
+                    fieldTypes[b] = 'f';
+                else
+                    fieldTypes[b] = 'i';
+            }
+        }
+    }
+
+    for (register int i = 0; i < records; ++i)
+    {
+        for (register int b = 0; b < fields; ++b)
+        {
+            QTableWidgetItem* item = new QTableWidgetItem();
+            switch (fieldTypes[b].toAscii())
+            {
+                case 's': // strings;
+                    item->setText(QString(strings + table[i * recordSize + b]));
+                    break;
+                case 'n': // index
+                case 'i': // ints
+                    item->setText(QString::number(table[i * recordSize + b]));
+                    break;
+                case 'f': // float
+                    item->setText(QString::number(*(float*)(table + i * recordSize + b)));
+                    break;
+                case 'x': // float
+                    break;
+            }
+            ui->tableWidget->setItem(i, b, item);
+        }
+    }
+    ui->tableWidget->setUpdatesEnabled(true);
+    ui->statusBar->showMessage("File Loaded");
+    loader.close();
+    delete [] table;
+    delete [] strings;
+}
+
+void DBCViewer::ReloadIntoTable()
+{
+    if (!lastFile.size())
+    {
+        QMessageBox message;
+        message.setText("You have not opened any file sucessfully..");
+        message.exec();
+        ui->statusBar->showMessage("Cannot open file " + lastFile);
+        return;
+    }
+    if (!loader.isOpen())
+        loader.close();
+    if (!loader.open(QIODevice::ReadOnly))
+    {
+        QMessageBox message;
+        message.setText("Cannot open file " + lastFile);
+        message.exec();
+        ui->statusBar->showMessage("Cannot open file " + lastFile);
+        return;
+    }
+
+    char title[5];
+    int records;
+    int fields;
+    int recordSize;
+    int stringsSize;
+
+    QDataStream stream(&loader);
+    stream.setByteOrder(QDataStream::LittleEndian);
+    stream.readRawData(title, 4);
+    if (qstrncmp(title, "WDBC", 4) != 0)
+    {
+        QMessageBox message;
+        message.setText("This it not DBC File !!");
+        message.exec();
+        ui->statusBar->showMessage("File is not DBC " + lastFile);
         return;
     }
     stream >> records;
@@ -66,7 +190,12 @@ void DBCViewer::LoadIntoTable(QString file)
     for (int i = ui->tableWidget->rowCount(); i < records; ++i)
         ui->tableWidget->insertRow(ui->tableWidget->rowCount());
 
-    stringsOffset = records * recordSize + 20;
+    for (int i = ui->tableWidget->columnCount(); i > fields; --i)
+        ui->tableWidget->removeColumn(ui->tableWidget->columnCount() - 1);
+
+    for (int i = ui->tableWidget->rowCount(); i > records; --i)
+        ui->tableWidget->removeRow(ui->tableWidget->rowCount() - 1);
+
 
     int* table = new int[records * recordSize];
 
@@ -79,46 +208,25 @@ void DBCViewer::LoadIntoTable(QString file)
     stream.readRawData(strings, stringsSize);
 
     ui->tableWidget->setUpdatesEnabled(false);
-    unsigned char fieldTypes[fields];
-    memset(fieldTypes, 1, fields);
-    fieldTypes[0] = 0;
-
-    for (register int i = 0; i < records; ++i)
-    {
-        for (register int b = 1; b < fields; ++b)
-        {
-            if (table[i * recordSize + b] > 0 &&
-                table[i * recordSize + b] < stringsSize  &&
-                *(strings + table[i * recordSize + b] - 1) == 0 &&
-                *(strings + table[i * recordSize + b]) >= 'A' &&
-                *(strings + table[i * recordSize + b]) <= 'z' &&
-                fieldTypes[b] == 1)
-                fieldTypes[b] = 1;
-            else
-            {
-                if (*(float*)(table + i * recordSize + b) - int(*(float*)(table + i * recordSize + b)) != 0 && log10(table[i * recordSize + b]) > 6)
-                    fieldTypes[b] = 2;
-                else
-                    fieldTypes[b] = 0;
-            }
-        }
-    }
 
     for (register int i = 0; i < records; ++i)
     {
         for (register int b = 0; b < fields; ++b)
         {
             QTableWidgetItem* item = new QTableWidgetItem();
-            switch (fieldTypes[b])
+            switch (fieldTypes[b].toAscii())
             {
-                case 1: // strings;
+                case 's': // strings;
                     item->setText(QString(strings + table[i * recordSize + b]));
                     break;
-                case 0: // ints
+                case 'n': // index
+                case 'i': // ints
                     item->setText(QString::number(table[i * recordSize + b]));
                     break;
-                case 2: // float
+                case 'f': // float
                     item->setText(QString::number(*(float*)(table + i * recordSize + b)));
+                    break;
+                case 'x': // float
                     break;
             }
             ui->tableWidget->setItem(i, b, item);
@@ -131,7 +239,7 @@ void DBCViewer::LoadIntoTable(QString file)
     delete [] strings;
 }
 
-void DBCViewer::on_tableWidget_currentCellChanged(int currentRow, int currentColumn, int previousRow, int previousColumn)
+void DBCViewer::on_tableWidget_currentCellChanged(int currentRow, int /* currentColumn */, int previousRow, int /* previousColumn */)
 {
     for (int i = 0; i < ui->tableWidget->columnCount(); ++i)
         if (QTableWidgetItem* item = ui->tableWidget->item(previousRow, i))
@@ -139,4 +247,27 @@ void DBCViewer::on_tableWidget_currentCellChanged(int currentRow, int currentCol
     for (int i = 0; i < ui->tableWidget->columnCount(); ++i)
         if (QTableWidgetItem* item = ui->tableWidget->item(currentRow, i))
             item->setBackground(QBrush(QColor(0xFF, 0x6E, 0x00)));
+}
+
+void DBCViewer::on_actionGet_triggered()
+{
+    QMessageBox msg(QMessageBox::Information, "Get Format String", "Actual format string is: '"+fieldTypes+"'", QMessageBox::Ok);
+    msg.exec();
+}
+
+void DBCViewer::on_actionSet_triggered()
+{
+    QString input = QInputDialog::getText(this, "Set Format String", "Enter Format String :");
+    if (input.size() != fieldTypes.size())
+    {
+        QMessageBox msg(QMessageBox::Information, "Invalid Format string", "Format string you have entered does not match length of actual format string", QMessageBox::Ok);
+        msg.exec();
+    }
+    else
+        fieldTypes = input;
+}
+
+void DBCViewer::on_actionReload_triggered()
+{
+    ReloadIntoTable();
 }
--- dbcviewer.h
+++ dbcviewer.h
@@ -4,6 +4,7 @@
 #include <QMainWindow>
 #include <QMessageBox>
 #include <QFileDialog>
+#include <QInputDialog>
 #include <QFile>
 #include <cmath>
 
@@ -25,11 +26,21 @@ private slots:
     void on_actionOpen_triggered();
 
     void LoadIntoTable(QString file);
+    void ReloadIntoTable();
 
     void on_tableWidget_currentCellChanged(int currentRow, int currentColumn, int previousRow, int previousColumn);
 
+    void on_actionGet_triggered();
+
+    void on_actionSet_triggered();
+
+    void on_actionReload_triggered();
+
 private:
     Ui::DBCViewer *ui;
+    QString lastFile;
+    QString fieldTypes;
+    QFile loader;
 };
 
 #endif // DBCVIEWER_H
